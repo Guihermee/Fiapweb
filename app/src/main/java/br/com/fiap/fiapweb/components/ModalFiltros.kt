a6ce8228ab1197c1b8046bc780b7c78dbd92d2ce
Nicolas Dutra <nicolas.nick.11@hotmail.com>
2024-06-01 19:37:55 -0300

    Modal de Filtros modificada para adcionar o chip

--- app/src/main/java/br/com/fiap/fiapweb/components/ModalFiltros.kt
+++ app/src/main/java/br/com/fiap/fiapweb/components/ModalFiltros.kt
@@ -1,17 +1,17 @@
 package br.com.fiap.fiapweb.components
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
-import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FilterChip
+import androidx.compose.material3.FilterChipDefaults
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
@@ -20,36 +20,53 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
-import br.com.fiap.fiapweb.R
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun ModalI(icon: ImageVector, label: String, onClick: () -> Unit) {
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .clickable(onClick = onClick)
-            .padding(vertical = 8.dp, horizontal = 16.dp),
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        Icon(
-            imageVector = icon,
-            contentDescription = null,
-            modifier = Modifier.size(24.dp)
-        )
-        Spacer(modifier = Modifier.width(16.dp))
-        Text(
-            text = label,
-            fontSize = 16.sp,
-            fontWeight = FontWeight.Medium
-        )
-    }
+fun ModalItems(icon: ImageVector, label: String, onClick: () -> Unit) {
+    var selected by remember { mutableStateOf(false) }
+
+    FilterChip(
+        onClick = { selected = !selected },
+        label = {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clickable(onClick = onClick)
+                    .padding(vertical = 8.dp, horizontal = 16.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(
+                    imageVector = icon,
+                    contentDescription = null,
+                    modifier = Modifier.size(24.dp)
+                )
+                Spacer(modifier = Modifier.width(16.dp))
+                Text(
+                    text = label,
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Medium
+                )
+            }
+        },
+        selected = selected,
+        leadingIcon = if (selected) {
+            {
+                Icon(
+                    imageVector = Icons.Filled.Done,
+                    contentDescription = "Done icon",
+                    modifier = Modifier.size(FilterChipDefaults.IconSize)
+                )
+            }
+        } else {
+            null
+        },
+    )
 }
 
 @Composable
@@ -72,23 +89,26 @@ fun ModalFiltros(
                 Column(
                     modifier = Modifier
                         .align(Alignment.Center)
-                        .background(MaterialTheme.colorScheme.background, shape = RoundedCornerShape(16.dp))
+                        .background(
+                            MaterialTheme.colorScheme.background,
+                            shape = RoundedCornerShape(16.dp)
+                        )
                         .padding(16.dp)
                         .width(300.dp),
                     horizontalAlignment = Alignment.CenterHorizontally,
                     verticalArrangement = Arrangement.spacedBy(16.dp)
                 ) {
-                    ModalI(
+                    ModalItems(
                         icon = Icons.Default.Email,
                         label = "Tudo",
                         onClick = { }
                     )
-                    ModalI(
+                    ModalItems(
                         icon = Icons.Default.MarkEmailUnread,
                         label = "Não lidos",
                         onClick = { }
                     )
-                    ModalI(
+                    ModalItems(
                         icon = Icons.Default.Star,
                         label = "Favoritos",
                         onClick = { }
