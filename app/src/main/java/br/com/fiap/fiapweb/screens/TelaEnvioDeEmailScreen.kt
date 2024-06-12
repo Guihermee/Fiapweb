1760fab354c0329248e6d4f465267b5eaec992c1
Thalita Zeid <thalita@MacBook-Air-de-Thalita.local>
2024-06-12 18:02:49 -0300

    adicionando botao de enviar

--- app/src/main/java/br/com/fiap/fiapweb/screens/TelaEnvioDeEmailScreen.kt
+++ app/src/main/java/br/com/fiap/fiapweb/screens/TelaEnvioDeEmailScreen.kt
@@ -1,16 +1,22 @@
 package br.com.fiap.fiapweb.screens
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.AutoAwesome
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material.icons.outlined.AutoAwesome
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -20,9 +26,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import br.com.fiap.fiapweb.R
 import br.com.fiap.fiapweb.components.EmailAdress
 import br.com.fiap.fiapweb.components.EmailBody
 import br.com.fiap.fiapweb.components.HeaderEscreverEmail
@@ -43,12 +51,36 @@ fun TelaEnvioDeEmailScreen(
     val ccoFieldValue by envioDeEmailViewModel.ccoFieldValue.observeAsState(initial = "")
     var showCcCcoFields by remember { mutableStateOf(false) }
 
+
     Scaffold(
+//        floatingActionButton = {
+//            FloatingActionButton(onClick = {navController.navigate("telaLeituraEmail")}) {
+//                Icon(imageVector = Icons.Outlined.AutoAwesome, contentDescription = "Generate Icon")
+//            }
+//        }
+
         floatingActionButton = {
-            FloatingActionButton(onClick = {navController.navigate("telaLeituraEmail")}) {
-                Icon(imageVector = Icons.Outlined.AutoAwesome, contentDescription = "Generate Icon")
+            Column(
+                horizontalAlignment = Alignment.End,
+                verticalArrangement = Arrangement.spacedBy(16.dp)
+            ) {
+                FloatingActionButton(
+                    onClick = { /* Ação do primeiro botão */ },
+                    containerColor = MaterialTheme.colorScheme.primary,
+                    contentColor = MaterialTheme.colorScheme.onPrimary
+                ) {  Icon(Icons.Default.PlayArrow, contentDescription = "Enviar")
+                }
+                FloatingActionButton(
+                    onClick = { /* Ação do segundo botão */ },
+                    containerColor = MaterialTheme.colorScheme.secondary,
+                    contentColor = MaterialTheme.colorScheme.onSecondary
+                ) {
+                    Icon(Icons.Default.AutoAwesome, contentDescription = "AI")
+                }
             }
-        },
+        }
+
+        ,
 
         topBar = { HeaderEscreverEmail(textContent = "") {
 
@@ -114,11 +146,6 @@ fun TelaEnvioDeEmailScreen(
                         .fillMaxWidth(),
                     keyboardType = KeyboardType.Text,
                     updateValue = { envioDeEmailViewModel.onEmailBodyFieldValueChanged(it) }
-                )
-
-                SendButton(
-                    modifier = Modifier
-                        .fillMaxWidth(),
                 )
             }
         }
